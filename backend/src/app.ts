12f3d493bf1d7f56afd4b1ecae0babb13a419039
AdalbertoAo <4dalbertosil@gmail.com>
2026-04-06 15:09:04 +0100

    feat(backend): add endpoint `Status`

---
+++ backend/src/app.ts
@@ -0,0 +1,36 @@
+import "dotenv/config"
+import {type Request, type Response} from "express"
+import express from "express"
+import prisma from "./lib/prisma.js";
+import type { PgStatResult } from "./interfaces/interface.js";
+
+
+const PORT = process.env.PORT_SERVER || 3000
+const app = express();
+
+
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+ app.get('/status', async (req:Request, res:Response)=>{
+
+  try {
+    const result:PgStatResult[] = await prisma.$queryRaw `SELECT count(*) FROM pg_stat_activity WHERE state = 'active';`;
+
+    if (!result || !result[0])
+        return res.status(500).json({error: "Falha  a retornar as instancias no db"})
+    else
+      res.json({
+              "Instancias no banco": Number(result[0].count),
+            });
+  } catch (error) {
+    return res.status(500).json({error: "Falha  na ligacao do banco de dados"})
+  }
+
+  })
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
---
+++ backend/src/interfaces/interface.ts
@@ -0,0 +1,3 @@
+export interface PgStatResult {
+  count: bigint
+}
