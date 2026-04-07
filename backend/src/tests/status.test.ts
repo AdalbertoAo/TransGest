49f19d40acb4e39aead795cc6684d4dd65610de4
AdalbertoAo <4dalbertosil@gmail.com>
2026-04-07 21:17:19 +0100

    feat(backend): add test group for endpoint `./status`

---
+++ backend/src/tests/status.test.ts
@@ -0,0 +1,42 @@
+import {expect, test, describe, expectTypeOf} from "vitest"
+import Request from "supertest"
+import app from "../app.js"
+import type { DatabaseStatus, PgStatResult } from "../interfaces/interface.js"
+import prisma from "../lib/prisma.js"
+
+
+
+
+describe("All tests for endpoint ./status", ()=>{
+
+    test("GET status code to 200",async ()=>{
+     const response =   await Request(app).get("/status");
+     expect(response.status).toBe(200)
+       
+    }, 15000)
+
+    test("response to endpoint is true", async ()=>{
+        let date = new Date()
+        let formatedDate = ("0"+date.getDate()+"/0"+(date.getMonth() +1 )+"/"+date.getFullYear())
+        const db= {
+            database: {
+                "timestamp": formatedDate,
+                "status": "ok",
+                "instancias no banco": 1
+            }
+           
+        }
+    
+        const response = await Request(app).get("/status");
+        const responseBody = response.body
+        console.log(db)
+        expect(responseBody).toMatchObject(db)
+        console.log(response.body)
+    })
+    test("if typeof to instancias is BigInt", async ()=>{
+        
+        const result:PgStatResult[] = await prisma.$queryRaw `SELECT count(*) FROM pg_stat_activity WHERE state = 'active';`;
+        
+            expect(result[0]?.count ).toBeTypeOf("bigint")
+    })
+})
